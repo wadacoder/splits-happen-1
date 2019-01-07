15ad7ebcca2ca032485534fbf9509cc9a46e4bd5
svwadadekar <suhas.wadadekar@gmail.com>
2019-01-06 21:01:49 -0500

    Improved program help information

--- src/main/java/scorer/BowlingScoreReporter.java
+++ src/main/java/scorer/BowlingScoreReporter.java
@@ -14,10 +14,17 @@ public class BowlingScoreReporter {
 	public static void main(String[] args) {
 		if (args != null && args.length == 1 && !args[0].trim().isEmpty()) {
 			Game game = new Game(args[0]);
-			System.out.println("Game recorded with rolls as: " + args[0]);
-			System.out.println("Total score: " + game.getScore());
+			System.out.println("\tGame recorded with rolls as: " + args[0]);
+			System.out.println("\tTotal score: " + game.getScore());
 		} else {
-			System.out.println("Please provide valid roll information");
+			System.out.println("\tPlease provide valid roll sequence for all 10 frames");
+			System.out.println("\t\t\"X\" indicates a strike");
+			System.out.println("\t\t\"/\" indicates a spare");
+			System.out.println("\t\t\"-\" indicates a miss");
+			System.out.println("\t\ta number indicates the number of pins knocked down in the roll");
+			System.out.println("\tExamples:");
+			System.out.println("\t\t2/32345/24X43546/XX4");
+			System.out.println("\t\tXXXXXXXXXX25");
 		}
 	}
 
